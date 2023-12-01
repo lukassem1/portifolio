482b6bff827040ccda9bb8e56d6f93a0ad8e9269
Lucas Oliveira Silva <lucas_sem@hotmail.com>
2023-12-01 15:09:04 -0300

    initial commit

---
+++ js/commands.js
@@ -0,0 +1,91 @@
+var youtube = "https://www.youtube.com/channel/UCNjuDgYGw19og5j5YKE3xnQ";
+var password = "lukassem1";
+var linkedin = "https://www.linkedin.com/in/lucas-oliveira-silva-gi/";
+var instagram = "https://www.instagram.com/lukassem1/";
+var github = "https://github.com/lukassem1/";
+var email = 'mailto:lucas_sem@hotmail.com';
+var datacamp = 'https://www.datacamp.com/portfolio/lukassem';
+var leetcode = 'https://leetcode.com/losilva/';
+var beecrowd = 'https://www.beecrowd.com.br/judge/pt/profile/7633';
+
+lucas = [
+  "<br>",
+  "Olá, eu sou o Lucas!👋",
+  "Desde de criança sempre fui apaixonado por tecnologia, jogos e computadores.",
+  "Por algum tempo tentei empreender, tive um bar por dois anos, e implantei varias tecnologias nele.",
+  "Porém sempre tive vontade de trabalhar diretamente com tecnologia. Quando estudei Gestão da informação.",
+  "criei a liga da gestão da informação que buscava conectar diretores e especialistas de grandes empresas",
+  "para palestrar case de sucesso para os alunos do curso de GI, e também para os alunos de outros cursos.",
+  "No final de 2022 decidi voltar para área de tecnologia, e comecei a estudar Python, SQL e banco de dados",
+  "Terminei o CS50 de Harvard, e comecei a estudar muito SQL, fiquei em 4 lugar na olímpiada de SQL",
+  "Me senti tão realizado e feliz, pensando que foi a melhor decisão que tomei na minha vida",
+  "Desde então tenho feito especialização pela universidade do Colorado em Banco de Dados",
+  "Fazendo o curso da datacamp de Data Engineering e estudando muito pelo leetcode e beecrowd",
+  "Com esse portfolio venho mostrar alguns dos meus trabalhos e projetos que fiz com muita dedicação",
+  "E quem sabe, poder trabalhar com vocês que estão aqui visitando",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/lucas-oliveira-silva-gi' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/lukassem1' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/lukassem1' + "</a>",
+  'datacamp       <a href="' + datacamp + '" target="_blank">datacamp/lukassem' + "</a>",
+  'leetcode       <a href="' + leetcode + '" target="_blank">leetcode/losilva' + "</a>",
+  'beecrowd       <a href="' + beecrowd + '" target="_blank">beecrowd/lukassem1' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Use somente se você for o admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Ainda em desenvolvimento, a maioria dos meus projetos estão offline, no GitHub, ou em criação.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">lucas</span>          Quem é o Lucas?',
+  '<span class="command">social</span>         Minhas redes sociais, me add lá',
+  '<span class="command">secret</span>         Descubra o password, não dou dicas',
+  '<span class="command">projects</span>       Veja meus projetos e me contrate :)',
+  '<span class="command">history</span>        Se caso quer saber o que você já digitou',
+  '<span class="command">help</span>           Você já sabe o que esse comando faz',
+  '<span class="command">email</span>          Meu email para contato, ofertas de emprego ou dicas',
+  '<span class="command">clear</span>          Tá cheio de coisa aqui, limpar a tela é uma boa',
+  '<span class="command">banner</span>         Animação do banner de abertura, caso queira ver de novo XD',
+  '<span class="command">whoami</span>         Quem sou eu? Quem é você no caso!',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Engenharia de dados, banco de dados e um pouco de viz.</span>',
+  '<span class="index">. . .</span>',
+
+"  .---------. | == |   	    __                             ____  ___            _           ",
+'  |.-"""""-.| |----|   	   / /   __  ___________ ______   / __ \\/ (_)   _____  (_)________ _',
+"  ||       || | == |   	  / /   / / / / ___/ __ '/ ___/  / / / / / / | / / _ \\/ / ___/ __ '/",
+"  ||       || |----|   	 / /___/ /_/ / /__/ /_/ (__  )  / /_/ / / /| |/ /  __/ / /  / /_/ / ",
+"  |'-.....-'| |::::|   	/_____/\\__,_/\\___/\\__,_/____/   \\____/_/_/ |___/\\___/_/_/   //\\__,_/  ",
+'  `"")---(""` |___.|',
+'/:::::::::::\\" _  ',
+"/:::=======:::\\`\\`\\		",
+'LOS `"""""""""""""`  '-'	',
+                                                               
+  
+
+  '<span class="color2">Bem vindo ao meu portifolio.</span>',
+  "<span class=\"color2\">Para explorar as opções digite</span> <span class=\"command\">help</span><span class=\"color2\">.</span>",
+];
